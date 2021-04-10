e129ad5bdc3351656fd088853247b263afcba080
jennder <der.je@husky.neu.edu>
2021-04-10 00:04:05 -0400

    initialize game play objects

---
+++ minimax_player.py
@@ -0,0 +1,24 @@
+"""A minimax player who maximizes their move assuming
+others are adversarial players.
+"""
+class MinimaxPlayer(PlayerInterface):
+
+    def __init__(self):
+        #TODO: needs game tree and its own color
+        pass
+
+    def get_move(self, board):
+        """
+        Returns the maximal move for this AI player.
+
+        Board -> Posn
+        """
+        pass
+
+    def set_color(self, color):
+        """
+        Let this player know their color.
+
+        Color -> void
+        """
+        pass
---
+++ player_interface.py
@@ -0,0 +1,27 @@
+"""Represents interface for any AI player
+"""
+class PlayerInterface:
+
+    def get_move(self):
+        """
+        Returns the maximal move for this AI player.
+
+        void -> Posn
+        """
+        pass
+
+    def update_with_move(self, posn):
+        """
+        Used to update internal state of player's game tree.
+
+        Posn -> void
+        """
+        pass
+
+    def set_color(self, color):
+        """
+        Let this player know their color.
+
+        Color -> void
+        """
+        pass
